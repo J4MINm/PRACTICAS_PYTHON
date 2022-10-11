3157e98f79fc13ea290509ea3697d04d6c4f39c6
J4MINlm <114263636+J4MINm@users.noreply.github.com>
2022-10-10 22:54:06 -0500

    P2-Suma
    Practica 2

---
+++ P2-Suma.py
@@ -0,0 +1,8 @@
+Numero1 = float (input("Ingresa primer numero: "));
+Numero2 = float (input("Ingresa segundo numero: "));
+
+# Operation
+Resultado = Numero1 + Numero2;
+
+# Print
+print(f"El resultado es: {Resultado}");
